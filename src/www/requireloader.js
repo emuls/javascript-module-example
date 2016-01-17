36212c40a5e75622c7226a1e0e0eb48749b8f908
Rex Jolley <emulsifier@gmail.com>
2016-01-17 11:44:42 -0700

    got angular to load via require

--- src/www/requireloader.js
+++ src/www/requireloader.js
@@ -3,6 +3,13 @@ requirejs.config({
     paths: {
         jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min',
         angular: '//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min'
+    },
+    shim: {
+        "jquery": { exports: "$" },
+        "angular" : {
+            exports : "angular",
+            deps: ["jquery"]
+        }
     }
 });
 
--- src/www/scripts/modules/angularComponents.js
+++ src/www/scripts/modules/angularComponents.js
@@ -1,5 +1,8 @@
 define(['angular', 'angularBlue','angularClick'],function(angular, angularBlue, angularClick) {
     console.log('angular components loaded');
+    console.log('ng: ' + !!angular);
+    console.log('blue: ' + !!angularBlue);
+    console.log('click: ' + !!angularClick);
     angular.module('moduleApp', []);
     angular.module('moduleApp').directive('makeBlue', angularBlue.directive);
     angular.module('moduleApp').directive('exaclickButton', angularClick.directive);
